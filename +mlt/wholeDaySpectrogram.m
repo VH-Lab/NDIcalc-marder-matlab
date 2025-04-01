f4b5445730d84db4dd1ba35c4543a4b49731f9a1
Jess Haley <jhaley@Jesss-MacBook-Pro.local>
2025-04-01 15:44:30 -0700

    added ability to use movzscore in wholeDaySpectrogram and wholeDayHeartBeat

--- +mlt/wholeDaySpectrogram.m
+++ +mlt/wholeDaySpectrogram.m
@@ -17,6 +17,7 @@ function [spec,f,ts] = wholeDaySpectrogram(S, options)
 % | f (0.1:0.1:10)            | The frequencies to examine               |
 % | windowTime (10)           | The window time in seconds               |
 % | downSample (2)            | Take every 'downSample' sample in answer |
+% | zscoreWindowTime (3600)   | The z-score time window in seconds       |
 % |----------------------------------------------------------------------|
 %
 
@@ -27,6 +28,7 @@ arguments
     options.f (1,:) double = 0.1:0.1:10
     options.windowTime (1,1) double = 10
     options.downSample (1,1) double = 2
+    options.zscoreWindowTime (1,1) double {mustBeNonnegative} = 3600
 end
 
 e = S.getelements('element.name',options.e_name,'element.reference',options.e_reference);
@@ -60,7 +62,12 @@ else
     t0t1 = et(1).t0_t1{idx};
     tr = ndi.time.timereference(e,ndi.time.clocktype('exp_global_time'),[],0);
     [d,t] = e.readtimeseries(tr,t0t1(1),t0t1(2));
-    d = zscore(d);
+    if options.zscoreWindowTime == 0
+        d = zscore(d);
+    else
+        t_date = datetime(t,'convertFrom','datenum');
+        d = mlt.movzscore(d,seconds(options.zscoreWindowTime),'SamplePoints',t_date);
+    end
     [spec,f,ts] = mlt.spectrogram(d,t,'frequencies',options.f, 'windowSizeTime', options.windowTime,'timeIsDatenum',true);
 end
 
