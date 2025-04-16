42d797bcd4a40cc33b7246540178626285fd9dc0
Jess Haley <jess@walthamdatascience.com>
2025-04-16 13:36:35 -0700

    Added ability to downsample newly acquired epochs

--- +mlt/graceSpectrogramsDoc.m
+++ +mlt/graceSpectrogramsDoc.m
@@ -10,10 +10,10 @@ p = S.getprobes('type','ppg');
 
 path = S.path();
 
-for i=1:numel(p),
+for i=1:numel(p)
     disp(['Checking to see if we have already downsampled ' p{i}.elementstring '...']);
     e = S.getelements('element.name',[p{i}.name '_lp_whole'],'element.reference',p{i}.reference);
-    if isempty(e),
+    if isempty(e)
         error(['No ''_lp'' version of ' p{i}.elementstring]);
     end
     [doc]=mlt.wholeDaySpectrogramDoc(S,'e_name',e{1}.name,'e_reference',e{1}.reference);
